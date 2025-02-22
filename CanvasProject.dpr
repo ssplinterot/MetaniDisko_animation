527b93e5fd8b60dd254218abd101985e48ddd2e9
mrDjadai <igor24122006@gmail.com>
2025-02-22 21:05:39 +0300

    AddedFrameSwiching

--- CanvasProject.dpr
+++ CanvasProject.dpr
@@ -1,14 +1,14 @@
 program CanvasProject;
 
 uses
-  System.StartUpCopy,
-  FMX.Forms,
+  Vcl.Forms,
   Animator in 'Animator.pas' {Form2};
 
 {$R *.res}
 
 begin
   Application.Initialize;
+  Application.MainFormOnTaskbar := True;
   Application.CreateForm(TForm2, Form2);
   Application.Run;
 end.
